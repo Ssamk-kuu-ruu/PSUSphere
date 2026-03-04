a0f4c4e5fca8db56c1917a3e2e9454e2dca657a4
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2026-03-04 12:41:32 +0800

    modified allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&fvv5%1zsw5+wrm171c!mjh@z4kvzdwb7*jjc1^673+@_otb2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['jrntrpsc.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
