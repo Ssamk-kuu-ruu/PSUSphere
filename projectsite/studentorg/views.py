d9d58a8cd7534576e8cda86d032e6c48da0d699e
kkuruqt <jiropasco05192002@gmail.com>
2026-05-31 17:41:28 +0800

    Implement Organization list view and template

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = 'home.html'
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization_list'
+    template_name = 'organization_list.html'
+    paginate_by = 5
+
